246a1f8d9968e03db8101aed2c135d3847b39b5d
Yumps <loganpalmer199721@hotmail.com>
2019-09-09 10:35:10 -0500

    initial migration

--- Manabu/Data/ApplicationDbContext.cs
+++ Manabu/Data/ApplicationDbContext.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Manabu.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,9 +10,70 @@ namespace Manabu.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Quiz> Quizzes { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<AnswerKey> AnswerKeys { get; set; }
+        public DbSet<FlashCard> FlashCards { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
+            // Create a new user for Identity Framework
+            ApplicationUser user = new ApplicationUser
+            {
+                FirstName = "admin",
+                LastName = "admin",
+                UserName = "admin@admin.com",
+                NormalizedUserName = "ADMIN@ADMIN.COM",
+                Email = "admin@admin.com",
+                NormalizedEmail = "ADMIN@ADMIN.COM",
+                EmailConfirmed = true,
+                LockoutEnabled = false,
+                SecurityStamp = "7f434309-a4d9-48e9-9ebb-8803db794577",
+                Id = "00000000-ffff-ffff-ffff-ffffffffffff"
+            };
+            var passwordHash = new PasswordHasher<ApplicationUser>();
+            user.PasswordHash = passwordHash.HashPassword(user, "Admin8*");
+            modelBuilder.Entity<ApplicationUser>().HasData(user);
+
+            modelBuilder.Entity<Quiz>().HasData(
+                new Quiz()
+                {
+                    Id = 1,
+                    Name = "Quiz 1"
+                }
+            );
+
+            modelBuilder.Entity<Question>().HasData(
+                new Question()
+                {
+                    Id = 1,
+                    Name = "や"
+                }
+            );
+
+            modelBuilder.Entity<AnswerKey>().HasData(
+                new AnswerKey()
+                {
+                    Id = 1,
+                    Name = "ya",
+                    QuestionId = 1
+                }
+            );
+
+            modelBuilder.Entity<FlashCard>().HasData(
+                new FlashCard()
+                {
+                    Id = 1,
+                    Question = "や",
+                    Answer = "ya",
+                    UserId = user.Id
+                }
+            );
         }
     }
 }
