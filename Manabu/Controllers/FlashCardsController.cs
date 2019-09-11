de3f161ffe19f6e7eb252a67b45f4283e68dc5ff
Yumps <loganpalmer199721@hotmail.com>
2019-09-11 09:29:02 -0500

    basic controllers created

---
+++ Manabu/Controllers/FlashCardsController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Manabu.Data;
+using Manabu.Models;
+
+namespace Manabu.Controllers
+{
+    public class FlashCardsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FlashCardsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: FlashCards
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.FlashCards.Include(f => f.User);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: FlashCards/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var flashCard = await _context.FlashCards
+                .Include(f => f.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (flashCard == null)
+            {
+                return NotFound();
+            }
+
+            return View(flashCard);
+        }
+
+        // GET: FlashCards/Create
+        public IActionResult Create()
+        {
+            ViewData["UserId"] = new SelectList(_context.ApplicationUsers, "Id", "Id");
+            return View();
+        }
+
+        // POST: FlashCards/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Question,Answer,UserId")] FlashCard flashCard)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(flashCard);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.ApplicationUsers, "Id", "Id", flashCard.UserId);
+            return View(flashCard);
+        }
+
+        // GET: FlashCards/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var flashCard = await _context.FlashCards.FindAsync(id);
+            if (flashCard == null)
+            {
+                return NotFound();
+            }
+            ViewData["UserId"] = new SelectList(_context.ApplicationUsers, "Id", "Id", flashCard.UserId);
+            return View(flashCard);
+        }
+
+        // POST: FlashCards/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Question,Answer,UserId")] FlashCard flashCard)
+        {
+            if (id != flashCard.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(flashCard);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FlashCardExists(flashCard.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.ApplicationUsers, "Id", "Id", flashCard.UserId);
+            return View(flashCard);
+        }
+
+        // GET: FlashCards/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var flashCard = await _context.FlashCards
+                .Include(f => f.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (flashCard == null)
+            {
+                return NotFound();
+            }
+
+            return View(flashCard);
+        }
+
+        // POST: FlashCards/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var flashCard = await _context.FlashCards.FindAsync(id);
+            _context.FlashCards.Remove(flashCard);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FlashCardExists(int id)
+        {
+            return _context.FlashCards.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Manabu/Models/ViewModels/QuizQuestions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Manabu.Models.ViewModels
+{
+    public class QuizQuestions
+    {
+        public int Id { get; set; }
+        public Quiz Quiz { get; set; }
+        public Question Question { get; set; }
+    }
+}
