50f083dfd9aac354618b40d69987644b470bcee4
Yumps <loganpalmer199721@hotmail.com>
2019-09-18 14:36:03 -0500

    added font awesome currently not working, finished quiz results controller

--- Manabu/Models/Quiz.cs
+++ Manabu/Models/Quiz.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,7 +13,8 @@ namespace Manabu.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
-
         public virtual ICollection<QuizQuestions> QuizQuestions { get; set; }
+        [NotMapped]
+        public int CorrectAnswers { get; set; }
     }
 }
